1b1f437ad0d66460303d517dee747015a5b32611
Anders Pearson <anders@columbia.edu>
2009-09-14 13:24:26 -0400

    forms should be included

---
+++ pageblocks/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.forms import widgets
+
+class AddTextBlockForm(forms.Form):
+    label = forms.CharField()
+    body = forms.CharField(widget=widgets.Textarea())
+
+class AddImageBlockForm(forms.Form):
+    label = forms.CharField()
+    image = forms.ImageField()
+    caption = forms.CharField(widget=widgets.Textarea())
+
+class AddHTMLBlockForm(forms.Form):
+    label = forms.CharField()
+    html = forms.CharField(widget=widgets.Textarea())
+
+class AddImagePullQuoteBlockForm(forms.Form):
+    label = forms.CharField()
+    image = forms.ImageField()
+    caption = forms.CharField(widget=widgets.Textarea())
