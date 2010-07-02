0a8aa90e046954accc1a7c0ca21e94d9b64f2a15
Anders Pearson <anders@columbia.edu>
2010-07-02 11:51:40 -0400

    somehow forgot to add the admin file

---
+++ pageblocks/admin.py
@@ -0,0 +1,9 @@
+from models import TextBlock, ImageBlock, HTMLBlock, PullQuoteBlock, ImagePullQuoteBlock
+
+from django.contrib import admin
+
+admin.site.register(TextBlock)
+admin.site.register(ImageBlock)
+admin.site.register(HTMLBlock)
+admin.site.register(PullQuoteBlock)
+admin.site.register(ImagePullQuoteBlock)
