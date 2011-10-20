4bd0c8e7053240ab2d54c78d6dce4610107a5dfb
Anders Pearson <anders@columbia.edu>
2011-10-20 15:45:43 -0400

    stupid manifest...

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pageblocks",
-    version="0.5.2",
+    version="0.5.3",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
