2d35810d7004d9bc2fddcea3274008d49f0db15b
Anders Pearson <anders@columbia.edu>
2012-06-13 11:20:41 -0400

    version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pageblocks",
-    version="0.5.3",
+    version="0.5.4",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
