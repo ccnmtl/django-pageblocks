80edb52e2a324e299c869ec8b8aa7c909d88fa63
Anders Pearson <anders@columbia.edu>
2013-09-03 11:41:23 -0400

    use jenkins to run tests so we can get coverage reports

---
+++ pageblocks/tests/__init__.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class DummyTest(TestCase):
+    def test_nothing(self):
+        self.assertTrue(1==1)
---
+++ runtests.py
@@ -0,0 +1,62 @@
+""" run tests for pagetree
+
+$ virtualenv ve
+$ ./ve/bin/pip install -r test_reqs.txt
+$ ./ve/bin/python runtests.py
+"""
+
+
+from django.conf import settings
+from django.core.management import call_command
+
+
+def main():
+    # Dynamically configure the Django settings with the minimum necessary to
+    # get Django running tests
+    settings.configure(
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'pagetree',
+            'pageblocks',
+            'django_nose',
+            'django.contrib.markup',
+            'django_jenkins',
+        ),
+        TEST_RUNNER = 'django_nose.NoseTestSuiteRunner',
+
+        NOSE_ARGS = [
+            '--with-coverage',
+            '--cover-package=pageblocks',
+        ],
+        JENKINS_TASKS = (
+            'django_jenkins.tasks.with_coverage',
+            'django_jenkins.tasks.django_tests',
+        ),
+        PROJECT_APPS = [
+            'pageblocks',
+        ],
+        COVERAGE_EXCLUDES_FOLDERS = ['migrations'],
+        ROOT_URLCONF = [],
+        PAGEBLOCKS = ['pagetree.TestBlock', ],
+        SOUTH_TESTS_MIGRATE=False,
+
+        # Django replaces this, but it still wants it. *shrugs*
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+                'HOST': '',
+                'PORT': '',
+                'USER': '',
+                'PASSWORD': '',
+                }
+            }
+    )
+
+    # Fire off the tests
+    call_command('jenkins')
+
+if __name__ == '__main__':
+    main()
