9975542d7af5bc98b0a38d9bd433daa618ce0bcc
Anders Pearson <anders@columbia.edu>
2014-05-29 15:33:29 -0400

    more reasonable size on lightbox thumbnail

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pageblocks",
-    version="0.5.12",
+    version="0.5.13",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
