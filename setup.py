ef23281813e5829de8647373e7b24852b50b0e87
Anders Pearson <anders@columbia.edu>
2016-05-03 14:30:13 +0100

    :bookmark: 0.5.16

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-pageblocks",
-    version="0.5.15",
+    version="0.5.16",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
