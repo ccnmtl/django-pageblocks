a66e674ace8987708e318b0f18bcc9c9639bb691
Nik Nyby <nikolas@gnu.org>
2016-11-25 14:47:08 -0500

    Remove sorl-dependent image blocks.
    These are in the django-sorlimageblock package. Projects that are using
    these image blocks will have to hold off on upgrading django-pageblocks
    until they migrate to django-sorlimageblock.

--- pageblocks/exportimport.py
+++ pageblocks/exportimport.py
@@ -1,7 +1,5 @@
 import os
-from django.core.files import File
-from models import TextBlock, HTMLBlock, PullQuoteBlock, ImageBlock
-from models import ImagePullQuoteBlock
+from models import TextBlock, HTMLBlock, PullQuoteBlock, SimpleImageBlock
 
 from pagetree_export import register_class as register
 
@@ -61,56 +59,13 @@ class PullQuote(Text):
         return b
 
 
-@register
-class Image(object):
-    block_class = ImageBlock
-    identifier = 'image'
-
-    def exporter(self, block, xmlfile, zipfile):
-        filename = os.path.basename(block.image.file.name)
-        filename = "pageblocks/%s-%s" % (block.pk, filename)
-        zipfile.write(block.image.file.name, arcname=filename)
-        print >> xmlfile, \
-            u"""<img src="%s" caption="%s" />""" % (
-                filename, block.caption)
-
-    def importer(self, node, zipfile):
-        children = node.getchildren()
-        assert len(children) == 1 and children[0].tag == "img"
-        path = children[0].get("src")
-        caption = children[0].get("caption")
-        file = zipfile.open(path)
-        file.size = zipfile.getinfo(path).file_size
-        b = ImageBlock(caption=caption, image='')
-        b.save_image(File(file))
-        b.save()
-        return b
-
-
-@register
-class ImagePullQuote(Image):
-    block_class = ImagePullQuoteBlock
-    identifier = 'imagepullquote'
-
-    def importer(self, node, zipfile):
-        children = node.getchildren()
-        assert len(children) == 1 and children[0].tag == "img"
-        path = children[0].get("src")
-        caption = children[0].get("caption")
-        file = zipfile.open(path)
-        file.size = zipfile.getinfo(path).file_size
-        b = ImagePullQuoteBlock(caption=caption, image='')
-        b.save_image(File(file))
-        b.save()
-        return b
-
 # statichtml exporters
 
 
 @register
-class Image(object):
-    block_class = ImageBlock
-    identifier = 'image'
+class SimpleImage(object):
+    block_class = SimpleImageBlock
+    identifier = 'simpleimage'
     export_type = 'statichtml'
 
     def exporter(self, block, xmlfile, zipfile):
